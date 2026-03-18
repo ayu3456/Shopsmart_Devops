c21abf9bf01071f70849f3373cc54b50086cacd9
ayu3456 <coaderayush1309@gmail.com>
2026-03-18 18:49:39 +0530

    refactor: integrate prettier and eslint for consistent formatting
    Modernized the linting configuration to use ESLint flat configs and Prettier for code style consistency across the monorepo.

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,20 +1,22 @@
-const js = require("@eslint/js");
-const globals = require("globals");
+const js = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
 
 module.exports = [
     js.configs.recommended,
+    prettier,
     {
         languageOptions: {
             ecmaVersion: 2022,
-            sourceType: "commonjs",
+            sourceType: 'commonjs',
             globals: {
                 ...globals.node,
                 ...globals.jest,
             },
         },
         rules: {
-            "no-unused-vars": "warn",
-            "no-console": "off",
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
         },
     },
 ];
