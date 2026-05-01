84f9a2780cbceb9aed6f0b2534af95e30f47baa2
ayu3456 <coaderayush1309@gmail.com>
2026-05-01 19:43:10 +0530

    fix: use LabRole instead of creating a new IAM role for AWS Academy environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,27 +65,9 @@ resource "aws_ecs_cluster" "shopsmart_cluster" {
   name = "shopsmart-cluster"
 }
 
-# IAM Role for Task Execution
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "shopsmart_ecs_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# Use existing AWS Academy LabRole
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
 
 # Default VPC and Subnets
@@ -128,7 +110,8 @@ resource "aws_ecs_task_definition" "shopsmart_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  execution_role_arn       = data.aws_iam_role.lab_role.arn
+  task_role_arn            = data.aws_iam_role.lab_role.arn
 
   container_definitions = jsonencode([
     {
