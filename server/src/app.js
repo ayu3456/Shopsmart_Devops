2484397d28856618a4638dbf26665bec2e3375a8
ayu3456 <coaderayush1309@gmail.com>
2026-05-01 19:35:22 +0530

    fix: use app.use for catch-all route instead of app.get('*') for Express 5 compatibility

--- server/src/app.js
+++ server/src/app.js
@@ -95,7 +95,7 @@ app.delete('/api/products/:id', async (req, res) => {
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
